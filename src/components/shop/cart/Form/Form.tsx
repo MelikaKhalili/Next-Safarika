13baa52056e2ecfb5e51558ef6204650a6106531
melikakhalili <melikakhalili2500@gmail.com>
2025-03-30 09:57:35 +0330

    Creating_ folders

---
+++ src/components/shop/cart/Form/Form.tsx
@@ -0,0 +1,7 @@
+export default function Form() {
+  return (
+    <div>
+      <p>Melika</p>
+    </div>
+  );
+}
---
+++ src/components/shop/ssingleProduct/singleProduct.tsx
@@ -0,0 +1,7 @@
+export default function SingleProduct() {
+  return (
+    <div>
+      <input className="border-2 border-red-500" type="text" />
+    </div>
+  );
+}
