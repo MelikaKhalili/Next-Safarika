1d4b39471c7d5b062698676a88723c75fd8475a7
melikakhalili <melikakhalili2500@gmail.com>
2025-04-20 19:18:10 +0330

    feat:origindestionationsection

---
+++ src/components/shop/singleProduct/origin-destination/origindestination.tsx
@@ -0,0 +1,8 @@
+export default function OriginAndDestination() {
+  return (
+    <div className="!border-b-1 !border-b-gray-300 mb-4 pb-6">
+      <h1 className="!text-xl !font-bold">مبدا و مقصد</h1>
+      {/* نقشه */}
+    </div>
+  );
+}
