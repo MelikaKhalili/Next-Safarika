1118e3a612fe603f6dfb02c45485803729e2b15d
melikakhalili <melikakhalili2500@gmail.com>
2025-04-20 19:16:38 +0330

    feat:standardKarttour

---
+++ src/components/shop/singleProduct/travelprepinfo/travelprepinfo.tsx
@@ -0,0 +1,11 @@
+export default function Travelprepinfo() {
+  return (
+    <div className="!border-b-1 !border-b-gray-300 mb-4 pb-6">
+      <h1 className="!text-xl !font-bold">توضیحات قبل از سفر</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+        inventore deserunt doloribus ratione vel laudantium.
+      </p>
+    </div>
+  );
+}
