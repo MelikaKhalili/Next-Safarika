f0e8134c2bd4abfd701d6caaf9ea820f2ec0482f
melikakhalili <melikakhalili2500@gmail.com>
2025-04-20 19:21:07 +0330

    fix:adding new sections to the single product page

--- src/components/shop/singleProduct/singleproduct.tsx
+++ src/components/shop/singleProduct/singleproduct.tsx
@@ -1,12 +1,21 @@
 import SwiperGallery from "../shared/swipergallery";
+import Cancellationpolicy from "./cancellationpolicy/cancellationpolicy";
+import Comments from "./comments/comments";
+import Conditions from "./conditions/Conditions";
+import Bestamenities from "./D/A-Button/bestamenities";
+import Essentials from "./essentials/essentials";
 import Excursions from "./excursions/excursions";
 import Features from "./features/features";
+import OriginAndDestination from "./origin-destination/origindestination";
+import Residenz from "./residenz/residenz";
 import "./singleproduct.css";
+import Standardkarttour from "./standardkarttour/standardkarttour";
 import TimingTour from "./touroverview/timingtour/timingtour";
 import Tourdetails from "./touroverview/tourdetails/tourdetails";
 import TourOverview from "./touroverview/TourOverview";
 import Toursales from "./toursales/toursales";
 import Tourservices from "./tourservices/tourservices";
+import Travelprepinfo from "./travelprepinfo/travelprepinfo";
 export default function SingleProductSection() {
   return (
     <div className="w-full flex relative">
@@ -24,6 +33,15 @@ export default function SingleProductSection() {
           <Excursions />
           <Tourservices />
           <Features />
+          <Residenz />
+          <OriginAndDestination />
+          <Bestamenities />
+          <Essentials />
+          <Travelprepinfo />
+          <Conditions />
+          <Cancellationpolicy />
+          <Standardkarttour />
+          <Comments />
         </div>
       </div>
     </div>
