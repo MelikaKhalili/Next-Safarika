df618c8bbcdd37a1c1a53325ffe499b01f61d5ce
melikakhalili <melikakhalili2500@gmail.com>
2025-04-20 19:19:49 +0330

    feat:cancellationpolicysection

---
+++ src/components/shop/singleProduct/cancellationpolicy/cancellationpolicy.tsx
@@ -0,0 +1,12 @@
+export default function Cancellationpolicy() {
+  return (
+    <div className="!border-b-1 !border-b-gray-300 mb-4 pb-6">
+      <h1 className="!text-xl !font-bold">قوانین کنسلی تور</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione
+        aperiam vel officia excepturi nemo rerum optio omnis illum
+        exercitationem explicabo.
+      </p>
+    </div>
+  );
+}
