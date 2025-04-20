b2a002d71d1cb1bfc30b11dee215f8668eadffa2
melikakhalili <melikakhalili2500@gmail.com>
2025-04-20 19:19:31 +0330

    feat:conditionssection

---
+++ src/components/shop/singleProduct/conditions/Conditions.tsx
@@ -0,0 +1,12 @@
+export default function Conditions() {
+  return (
+    <div className="!border-b-1 !border-b-gray-300 mb-4 pb-6">
+      <h1 className="!text-xl !font-bold">شرایط و مقررات</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse sint
+        aliquam aliquid fuga provident voluptatibus animi nemo itaque.
+        Laboriosam, totam.
+      </p>
+    </div>
+  );
+}
