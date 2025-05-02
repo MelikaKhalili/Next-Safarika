fd71a5a4cc40873ea4edae715b1f86a750dc8c0d
melikakhalili <melikakhalili2500@gmail.com>
2025-05-02 05:34:53 +0330

    feat:Create an Add Product button

--- src/components/dash/products/buttonaddnewproduct/buttonAddNewProduct.tsx
+++ src/components/dash/products/buttonaddnewproduct/buttonAddNewProduct.tsx
@@ -2,12 +2,20 @@ import { ButtonAddNewProductProps } from "../types";
 
 export default function ButtonAddNewProduct({
   setIsOpenModalAddProduct,
+  editingProduct,
+  setEditingProduct,
+  isEditMode,
+  setIsEditMode,
 }: ButtonAddNewProductProps) {
   return (
     <div>
       <button
         style={{ backgroundColor: "var(--color-primary)" }}
-        onClick={() => setIsOpenModalAddProduct(true)}
+        onClick={() => {
+          setIsOpenModalAddProduct(true);
+          setEditingProduct(null);
+          setIsEditMode(false);
+        }}
         className=" !text-white !px-8 !py-2  !text-sm cursor-pointer rounded-3xl"
       >
         افزودن محصول
